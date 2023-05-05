d40cdd095bee48313e1843e959e75965fb19ea00
LasithaE <lasithaeaswaran@gmail.com>
2023-05-06 02:28:19 +0530

    feat: project links added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { HiOutlineExternalLink } from "react-icons/hi";
 
 const ProjectCard = ({ cardvalue }) => {
   return (
     <div className="border-solid flex flex-col justify-between bg-cardColorLight p-4 rounded-xl h-[40%]">
       <div className="px-6 pt-6">
-        <div className="text-xl font-black pb-3">{cardvalue.name}</div>
+        <div className="flex flex-row">
+          <div className="text-xl font-black pb-3">{cardvalue.name}</div>
+          <a href={cardvalue.link} target="_blank" className="pt-[5px] pl-3">
+            <HiOutlineExternalLink />
+          </a>
+        </div>
         <div className="text-lg font-normal">{cardvalue.desc}</div>
       </div>
       <div className="flex flex-row px-6 bottom-0 pb-6">
